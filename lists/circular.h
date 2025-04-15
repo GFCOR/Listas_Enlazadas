59af8093d736c55c79d2c7de55ba7fefc5042a97
salaverryyy <david.salaverry@utec.edu.pe>
2025-04-15 18:56:45 -0500

    forward hecho

--- lists/circular.h
+++ lists/circular.h
@@ -13,202 +13,92 @@ class CircularList : public List<T> {
             Node* prev;
 
             Node(){ 
-
-                next = nullptr;
-                prev = nullptr;
+                // TODO
             }
 
             Node(T value){
-                data = value;
-                next = nullptr;
-                prev = nullptr;
+                // TODO
             }
 
             void killSelf(){
-                delete this;
-            }
+                // TODO      
+            }    
         };
 
     private:
         Node* head;//sentinel
-        int nodes;
+        int nodes; 
 
     public:
-        CircularList() : List<T>() {
-          head = new Node();
-          head->next = head;
-          head->prev = head;
-          nodes = 0;
-        }
+        CircularList() : List<T>() { }
 
         ~CircularList(){
-                clear();      // borra todos los nodos con pop_front
-                delete head;  // finalmente borra el centinela
-            }
-         T front(){
-           return head->next->data;
+           // TODO
+        }       
 
+         T front(){
+            throw ("sin definir");
         }
 
         T back(){
-            Node* p = head;
-            while (p->next!=head){
-              p = p->next;
+            throw ("sin definir");
         }
-        return p->data;
-        }
+
         void push_front(T data){
-            Node* newNode = new Node(data);
-            newNode->next = head->next;
-            newNode->prev = head;
-            head->next = newNode;
-            nodes++;
-            newNode->next->prev = newNode;
+            throw ("sin definir");
         }
 
         void push_back(T data){
-            Node* newNode = new Node(data);
-            newNode->next = head;
-            newNode->prev = head->prev;
-            head->prev = newNode;
-            nodes++;
-            newNode->prev->next = newNode;
+            throw ("sin definir");
         }
 
         T pop_front(){
-            if (head->next == head){
-              throw std::out_of_range("List is empty");
-            }
-            Node* temp = head->next;
-            head->next = temp->next;
-            temp->next->prev = head;
-            T data = temp->data;
-            temp->killSelf();
-            nodes--;
-            return data;
+            throw ("sin definir");
         }
 
         T pop_back(){
-            if (head->prev == head){
-              throw std::out_of_range("List is empty");
-            }
-            Node* temp = head->prev;
-            head->prev = temp->prev;
-            temp->prev->next = head;
-            T data = temp->data;
-            temp->killSelf();
-            nodes--;
-            return data;
+            throw ("sin definir");
         }
+
         T insert(T data, int pos){
-            if (pos < 0 || pos >= nodes) {
-                throw std::out_of_range("out of range");
-            }
-            if (pos == nodes) {
-                push_back(data);
-                return;
-            }
-            Node* newNode = new Node(data);
-            Node* temp = head->next;
-            for (int i = 0; i < pos-1; i++){
-              temp = temp->next;
-            }
-            newNode->next = temp->next;
-            newNode->prev = temp;
-            temp->next->prev = newNode;
-            temp->next = newNode;
-            nodes++;
-            return data;
+            throw ("sin definir");
         }
 
         bool remove(int pos){
-            if (pos < 0 || pos >= nodes) {
-                throw std::out_of_range("out of range");
-            }
-
-            Node* temp = head->next;
-            for (int i = 0; i < pos; i++){
-              temp = temp->next;
-            }
-            temp->next->prev = temp->prev;
-            temp->prev->next = temp->next;
-            temp->killSelf();
-            nodes--;
-            return true;
+            throw ("sin definir");
         }
 
         T& operator[](int pos){
-            if (pos < 0 || pos >= nodes) {
-                throw std::out_of_range("out of range");
-            }
-
-            Node* temp = head->next;
-            for (int i = 0; i < pos; i++){
-              temp = temp->next;
-            }
-            return temp->data;
+            throw ("sin definir");
         }
 
         bool is_empty(){
-            if (head->next == head){
-              return true;
-            }
-            return false;
+            throw ("sin definir");
         }
 
         int size(){
-            return nodes;
+            throw ("sin definir");
         }
 
         void clear(){
-            if (head->next == head){
-              return;
-            }
-            while (head->next != head){
-              pop_front();
-            }
+            throw ("sin definir");
+        }
+        
+        void sort(){
+            throw ("sin definir");
         }
 
-        void sort(){
-                if (nodes < 2) return;
-
-                bool swapped;
-                do {
-                    swapped = false;
-                    Node* current = head->next;
-                    while (current->next != head) {
-                        if (current->data > current->next->data) {
-                            std::swap(current->data, current->next->data);
-                            swapped = true;
-                        }
-                        current = current->next;
-                    }
-                } while (swapped);
-            }
-
-
         bool is_sorted(){
-            Node* temp = head->next;
-            while (temp->next != head){
-                if (temp->data > temp->next->data){
-                    return false;
-                }
-                temp = temp->next;
-            }
-            return true;
+            throw ("sin definir");
+        }
 
-}
         void reverse(){
-            Node* current = head;
-            do {
-                swap(current->next, current->prev);
-                current = current->prev;
-            } while (current != head);
-}
+            throw ("sin definir");
+        }
 
         std::string name(){
             return "ForwardList";
-    }
+        }
 };
 
 #endif
--- lists/forward.h
+++ lists/forward.h
@@ -3,102 +3,219 @@
 #include <iostream>
 #include "list.h"
 
-// TODO: Implement all methods
 template <typename T>
 class ForwardList : public List<T> {
-    private:
-        struct Node {
-            T data;
-            Node* next;
+private:
+    struct Node {
+        T data;
+        Node* next;
 
-            Node(){ 
-                // TODO
+        Node() {
+            next = nullptr;
+        }
+
+        Node(T value) {
+            data = value;
+            next = nullptr;
+        }
+
+        void killSelf() {
+            delete this;
+        }
+    };
+
+private:
+    Node* head;
+    int nodes;
+
+public:
+    ForwardList() : List<T>(), head(nullptr), nodes(0) {}
+
+    ~ForwardList() {
+        clear();
+    }
+
+    T front() {
+        if (head == nullptr) {
+            throw std::out_of_range("List is empty");
+        }
+        return head->data;
+    }
+
+    T back() {
+        if (head == nullptr) {
+            throw std::out_of_range("List is empty");
+        }
+        Node* temp = head;
+        while (temp->next != nullptr) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    void push_front(T data) {
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+        nodes++;
+    }
+
+    void push_back(T data) {
+        Node* newNode = new Node(data);
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
             }
+            temp->next = newNode;
+        }
+        nodes++;
+    }
 
-            Node(T value){
-                // TODO
+    T pop_front() {
+        if (head == nullptr) {
+            throw std::out_of_range("List is empty");
+        }
+        Node* temp = head;
+        head = head->next;
+        T data = temp->data;
+        temp->killSelf();
+        nodes--;
+        return data;
+    }
+
+    T pop_back() {
+        if (head == nullptr) {
+            throw std::out_of_range("List is empty");
+        }
+        if (head->next == nullptr) {
+            T data = head->data;
+            delete head;
+            head = nullptr;
+            nodes--;
+            return data;
+        }
+        Node* temp = head;
+        while (temp->next->next != nullptr) {
+            temp = temp->next;
+        }
+        T data = temp->next->data;
+        delete temp->next;
+        temp->next = nullptr;
+        nodes--;
+        return data;
+    }
+
+    T insert(T data, int pos) {
+        if (pos < 0 || pos > nodes) {
+            throw std::out_of_range("out of range");
+        }
+        if (pos == 0) {
+            push_front(data);
+            return data;
+        }
+        Node* newNode = new Node(data);
+        Node* temp = head;
+        for (int i = 0; i < pos - 1; i++) {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+        nodes++;
+        return data;
+    }
+
+    bool remove(int pos) {
+        if (pos < 0 || pos >= nodes) {
+            throw std::out_of_range("out of range");
+        }
+        if (pos == 0) {
+            pop_front();
+            return true;
+        }
+        Node* temp = head;
+        for (int i = 0; i < pos - 1; i++) {
+            temp = temp->next;
+        }
+        Node* nodeToRemove = temp->next;
+        temp->next = temp->next->next;
+        nodeToRemove->killSelf();
+        nodes--;
+        return true;
+    }
+
+    T& operator[](int pos) {
+        if (pos < 0 || pos >= nodes) {
+            throw std::out_of_range("out of range");
+        }
+        Node* temp = head;
+        for (int i = 0; i < pos; i++) {
+            temp = temp->next;
+        }
+        return temp->data;
+    }
+
+    bool is_empty() {
+        return head == nullptr;
+    }
+
+    int size() {
+        return nodes;
+    }
+
+    void clear() {
+        while (!is_empty()) {
+            pop_front();
+        }
+    }
+
+    void sort() {
+
+        if (nodes < 2) return;
+
+        bool swapped;
+        do {
+            swapped = false;
+            Node* current = head;
+            while (current != nullptr && current->next != nullptr) {
+                if (current->data > current->next->data) {
+                    std::swap(current->data, current->next->data);
+                    swapped = true;
+                }
+                current = current->next;
             }
+        } while (swapped);
+    }
 
-            void killSelf(){
-                // TODO      
-            }    
-        };
-
-    private:
-        Node* head;
-        int nodes;
-
-    public:
-        ForwardList() : List<T>() {}
-
-        ~ForwardList(){ 
-         	// TODO            
+    bool is_sorted() {
+        Node* temp = head;
+        while (temp != nullptr && temp->next != nullptr) {
+            if (temp->data > temp->next->data) {
+                return false;
+            }
+            temp = temp->next;
         }
+        return true;
+    }
 
-        T front(){
-            throw ("sin definir");
+    void reverse() {
+        Node* current = head;
+        Node* prev = nullptr;
+        Node* next = nullptr;
+        while (current != nullptr) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
         }
+        head = prev;
+    }
 
-        T back(){
-            throw ("sin definir");
-        }
-
-        void push_front(T data){
-            throw ("sin definir");
-        }
-
-        void push_back(T data){
-            throw ("sin definir");
-        }
-
-        T pop_front(){
-            throw ("sin definir");
-        }
-
-        T pop_back(){
-            throw ("sin definir");
-        }
-
-        T insert(T data, int pos){
-            throw ("sin definir");
-        }
-
-        bool remove(int pos){
-            throw ("sin definir");
-        }
-
-        T& operator[](int pos){
-            throw ("sin definir");
-        }
-
-        bool is_empty(){
-            throw ("sin definir");
-        }
-
-        int size(){
-            throw ("sin definir");
-        }
-
-        void clear(){
-            throw ("sin definir");
-        }
-        
-        void sort(){
-            throw ("sin definir");
-        }
-
-        bool is_sorted(){
-            throw ("sin definir");
-        }
-
-        void reverse(){
-            throw ("sin definir");
-        }
-
-        std::string name(){
-            return "ForwardList";
-        }
-        
+    std::string name() {
+        return "ForwardList";
+    }
 };
 
-#endif
+#endif // FORWARD_H
